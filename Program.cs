763f6a2f693136fed54a1523c0c5a988a360a2f5
chan18 <chan18@users.noreply.github.com>
2020-02-29 12:51:47 +0530

    fixed edge case

--- Program.cs
+++ Program.cs
@@ -23,15 +23,17 @@ namespace interview
         {
             Program program = new Program(2);
 
-            var lorem = new Bogus.DataSets.Lorem("en");            
+            var lorem = new Bogus.DataSets.Lorem("en");
             var testUser = new Faker<PlayerDetails>()
                                 .StrictMode(true)
                                 .RuleFor(o => o.UserName, f => lorem.Word())
                                 .RuleFor(o => o.Rating, f => f.Random.Number(300,3000));
 
-           program.players =  testUser.Generate(6);
+           program.players =  testUser.Generate(100);
 
            program.players  = program.players.OrderBy(x => x.Rating).ToList();
+
+           program.match.teams = new List<List<PlayerDetails>>();
 
            for (int i = 0; i < program.numberOfTeams; i++)
            {
@@ -41,44 +43,63 @@ namespace interview
            Console.WriteLine(program.match);
         }
 
-        static List<PlayerDetails> BuildTeam(Program program,int numberOfTeams) 
+        static List<PlayerDetails> BuildTeam(Program program,int numberOfTeams)
         {
             List<PlayerDetails> temp = new List<PlayerDetails>();
             var numberOfplayersPerTeam = program.players.Count/numberOfTeams;
 
             for (int index = 0; index < program.players.Count; index++)
             {
-                if (numberOfplayersPerTeam ==  (index + 1))
-                {
-                    return temp;
-                }
-
                 if (program.players.ElementAtOrDefault(index) != null)
                 {
-                    temp.Add(program.players[index]);
-                    program.players.RemoveAt(index);
-
-                    if (program.players.ElementAtOrDefault(program.players.Count() - 1) != null)
+                    if (numberOfplayersPerTeam != temp.Count)
                     {
-                        temp.Add(program.players[program.players.Count() - 1]);
-                        program.players.RemoveAt(program.players.Count() - 1);
+                        temp.Add(program.players[index]);
+                        program.players.RemoveAt(index);
+                    }
+                    else
+                    {
+                        return temp;
+                    }
+
+                    if (numberOfplayersPerTeam != temp.Count)
+                    {
+                        if (program.players.ElementAtOrDefault(program.players.Count() - 1) != null)
+                        {
+                            temp.Add(program.players[program.players.Count() - 1]);
+                            program.players.RemoveAt(program.players.Count() - 1);
+                        }
+                    }
+                    else
+                    {
+                        return temp;
                     }
                 }
+            }
+
+            // edge case
+            if (program.players.Count != 0)
+            {
+                for  (int index = 0; index < program.players.Count; index++)
+                {
+                    temp.Add(program.players[index]);
+                }
+
+                program.players = new List<PlayerDetails>();;
             }
 
             return temp;
         }
     }
 
-    internal class Match
+    public class Match
     {
         public List<List<PlayerDetails>> teams {get; set;}
     }
 
-    internal class PlayerDetails
+    public class PlayerDetails
     {
         public string UserName {get; set;}
         public int Rating {get;set;}
     }
 }
-        
